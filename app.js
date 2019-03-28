c491d9db73ae53431b2d9f970952d35307c7871c
Summer Feiler <47381138+spfeiler@users.noreply.github.com>
2019-03-28 15:05:16 -0500

    added partials and sessions March 28 2019

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
 const express = require('express')
-const mustacheExpress = require('mustache-express')
 const bodyParser = require('body-parser')
+const mustacheExpress = require('mustache-express')
+const path = require('path')
+
+var session = require('express-session')
+
 const app = express()
+
+const VIEWS_PATH = path.join(__dirname, '/views')
+
+app.use(session( {
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.engine('mustache', mustacheExpress())
+app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
 
 app.set('views', './views')
 
 app.set('view engine', 'mustache')
 
-let trips = []
+let users = []
+let persistedUser = {}
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/register', (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+
+  let user = {username: username, password: password, userTrips: []}
+  users.push(user)
+  console.log(users)
+
+  res.render('login', {registeredMessage: "You are Registered! Please Login Below!"})
+})
+
+app.post('/login', (req, res) => {
+  let username = req.body.username
+  let password = req.body.password
+
+  let user = {username: username, password: password, userTrips: []}
+
+  persistedUser = users.find((user) => {
+    return user.username == username && user.password == password
+  })
+  if(persistedUser) {
+    if(req.session) {
+      req.session.username = persistedUser.username
+      res.redirect('/trips')
+    }
+  } else {
+    res.render('login', {invalidMessage: "Invalid Credentials! Please Try Again!"})
+  }
+})
+
+app.post('/signout', (req, res) => {
+  req.session.destroy(function(err) {
+    if(err) {
+      console.log(err)
+    } else {
+      res.redirect('/login')
+    }
+  })
+})
 
 app.get('/trips', (req, res) => {
-  res.render('trips', {tripList: trips})
+  res.render('trips', {username: req.session.username, trips: persistedUser.userTrips})
 })
 
 app.get('/addtrip', (req, res) => {
@@ -28,8 +85,8 @@ app.post('/addtrip', (req, res) => {
   let image = req.body.image
   let tripID = guid()
 
-  let trip = {destination: destination, departure: departureDate, return: returnDate, image: image, tripID: tripID}
-  trips.push(trip)
+  let triplist = ({destination: destination, departure: departureDate, return: returnDate, image: image, tripID: tripID})
+  persistedUser.userTrips.push(triplist)
 
   res.redirect('/trips')
 })
@@ -47,15 +104,11 @@ function guid(length) {
 
 app.post('/deletetrip', (req, res) => {
   let deleteTrip = req.body.tripID
-
-  trips = trips.filter(function(trip) {
+  
+  persistedUser.userTrips = persistedUser.userTrips.filter(function(trip) {
     return trip.tripID != deleteTrip
   })
-  res.render('trips', {tripList: trips})
-})
-
-app.post('/returnaddtrip', (req, res) => {
-  res.redirect('/addtrip')
+  res.render('trips', {username: req.session.username, trips: persistedUser.userTrips})
 })
 
 app.listen(3000, () => {
